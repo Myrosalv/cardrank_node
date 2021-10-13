66000fe708848a16c03ae362b9caf04db99fb2e0
Myroslav <myroslav.kogut2007@gmail.com>
2021-10-13 22:04:30 +0300

    thirdcommit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const { request, response } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const { Schema } = mongoose;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,32 +11,49 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/cardrank', { useNewUrlParser: true });
 
-const cardsSchema = {
-   name: String,
-   issuer: String,
+const cardsSchema = new Schema({
+   name: { type: String, index: true },
+   issuer: { type: String, index: true },
    apr: String,
-   Fee: String,
+   Fee: { type: String, index: true },
    travel: String,
    dining: String,
    other: String,
-}
+});
 
+cardsSchema.index({ name: 1, issuer: 1, Fee: 1 });
 const Card = mongoose.model('Card', cardsSchema);
 
 app.post('/form', async (req, res) => {
-   let newCard = new Card({
-      name: req.body.name,
-      issuer: req.body.issuer,
-      apr: req.body.apr,
-      Fee: req.body.Fee,
-      travel: req.body.travel,
-      dining: req.body.dining,
-      other: req.body.other,
-   });
-   newCard.save();
+   try {
+      let newCard = new Card({
+         name: req.body.name,
+         issuer: req.body.issuer,
+         apr: req.body.apr,
+         Fee: req.body.Fee,
+         travel: req.body.travel,
+         dining: req.body.dining,
+         other: req.body.other,
+      });
+      newCard.save();
+   } catch (error) {
+      res.status(500).json({
+         success: false,
+         error
+      });
+   }
 
-   const cards = await Card.find({});
-   res.status(200).json(cards);
+   res.status(200).json({
+      success: true
+   });
+});
+
+app.post('/getCard', async (req, res) => {
+   const payload = req.body.payload.trim();
+   const search = await Card.find({ name: { $regex: new RegExp('^' + payload + '.*', 'i') } }).exec();
+   //const search = await Card.find({ $text: { $search: payload } });
+
+   res.send({ payload: search });
 });
 
 app.get('/', (req, res) => {
@@ -46,9 +64,6 @@ app.get('/', (req, res) => {
    })
 })
 
-app.get('/search', async (req, res) => {
-
-});
 
 app.listen(3000, function () {
    console.log('server is running');
